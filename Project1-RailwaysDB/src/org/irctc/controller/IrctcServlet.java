574bab85cc08e8daa042fa815e45ec51b493f233
SkyTech <SkyTech@SkyTech>
2018-06-27 11:59:19 +0530

    Commit 2

--- Project1-RailwaysDB/src/org/irctc/controller/IrctcServlet.java
+++ Project1-RailwaysDB/src/org/irctc/controller/IrctcServlet.java
@@ -2,12 +2,10 @@ package org.irctc.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/IrctcServlet")
 public class IrctcServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
